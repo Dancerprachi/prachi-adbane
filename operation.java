ec98114f21b509dcb0635ecbf0921df1c0465418
Dancerprachi <prachiadbane@gmail.com>
2023-08-26 12:09:14 +0530

    fFirst java code

---
+++ operation.java
@@ -0,0 +1,19 @@
+public class operation 
+{
+    public static void main(String[] args) 
+    {
+        int a,b,sum,sub,mul,div;
+        a=10;
+        b=20;
+        sum=a+b;
+        System.out.println("sum is\t"+sum);
+        sub=a-b;
+        System.out.println("sub is\t"+sub);
+        mul=a*b;
+        System.out.println("mul is\t"+mul);
+        div=a/b;
+        System.out.println("div is\t"+div);
+        
+    }
+    
+}
